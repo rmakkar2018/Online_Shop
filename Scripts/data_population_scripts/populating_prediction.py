736f3a80d1c0a13a6cedae566d114051308c6852
Noob_001 <rmakkar2018@users.noreply.github.com>
2020-04-27 20:35:27 +0530

    prediction table population
    prediction table population using above script (it will give different tables with each run so use only once becuase of the use of random library )

---
+++ Scripts/data_population_scripts/populating_prediction.py
@@ -0,0 +1,48 @@
+import mysql.connector as sql
+from datetime import datetime
+from random import *
+
+db = sql.connect(
+	host="localhost",
+	user = "root",
+	passwd = "himraj18",
+	database = "project67"
+)
+cursor=db.cursor()
+#populating prediction table using this code
+
+#jan feb march april may june july august sep  oct  nov  dec
+# 1   2   3     4     5    6   7     8     9   10    11   12
+# 31      28          31       31    31        31         31 
+
+for year in range(2000,2020):
+	is_leap_year=False
+	if(year%4==0 and year%100!=0):
+		is_leap_year=True
+	if(year%4==0 and year%100==0 and year%400==0):
+		is_leap_year=True
+
+	for month in range(1,13):
+		total_no_days=30
+		if(month==1 or month==5 or month==7 or month==8 or month==10 or month==12):
+			total_no_days=31
+		if(month==3):
+			if(is_leap_year):
+				total_no_days=29
+			else:
+				total_no_days=28
+		for day in range(1,total_no_days+1):
+			today_date=str(day)+"-"+str(month)+"-"+str(year)
+			for item_id in range(6,19):
+				attr=[]
+				attr.append(str(today_date))
+				attr.append(str(item_id))
+				sold_num=randint(10,100)
+				attr.append(sold_num)
+				query="Insert Into prediction Values (%s,%s,%s);"
+				cursor.execute(query,attr)
+				db.commit()
+
+
+
+
