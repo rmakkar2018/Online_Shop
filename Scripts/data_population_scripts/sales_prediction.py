7960dcdb2d3ed2c9c1e64ad355c19325178ad2b0
himanshu18038 <himanshu18038@iiitd.ac.in>
2020-04-28 21:22:05 +0530

    Update sales_prediction.py

--- Scripts/data_population_scripts/sales_prediction.py
+++ Scripts/data_population_scripts/sales_prediction.py
@@ -3,9 +3,14 @@ from sklearn.linear_model import LinearRegression
 import mysql.connector as sql
 from datetime import datetime
 
-
+def fetchdetails(cursor):
+	l=[]
+	for i in cursor:
+		l.append(i)
+	return l
 
 def predict_sales_for_item(month,year,Item_ID):
+
 	X=[]
 	y=[]
 	# now read from database and split the month and year part and add those values in X, and correspoding values of Quantityin y
@@ -19,16 +24,16 @@ def predict_sales_for_item(month,year,Item_ID):
 	cursor=db.cursor()
 	query="Select Date, Quantity from prediction where Item_ID="+str(Item_ID)+";"
 	cursor.execute(query)
-	lst=db.fetchdetails(cursor)
+	lst=fetchdetails(cursor)
 	ptr=0
 	while(ptr<len(lst)):
-		date=datetime.date(lst[ptr][0])
+		date=lst[ptr][0]
 		Quantity=int (lst[ptr][1])
 		month=int(date.month)
 		year=int(date.year)
 		ptr+=1
 		while(ptr<len(lst)):
-			next_date=datetime.date(lst[ptr][0])
+			next_date=lst[ptr][0]
 			next_month=int(next_date.month)
 			next_year=int(next_date.year)
 			if(next_month==month and next_year==year):
@@ -45,8 +50,10 @@ def predict_sales_for_item(month,year,Item_ID):
 	X = np.array(X)
 	y = np.array(y)
 	reg = LinearRegression().fit(X, y)
-	print("how good is fitting: "+reg.score(X, y))
+	print("how good is fitting: "+str(reg.score(X, y)))
 	# print(reg.coef_)
 	# print(reg.intercept_)
 	value=reg.predict(np.array([[int(month), int(year)]]))
-	print("The predicted value of "+value[0])
+	print("The predicted value of "+str(value[0]))
+
+predict_sales_for_item(3,2021,12)
